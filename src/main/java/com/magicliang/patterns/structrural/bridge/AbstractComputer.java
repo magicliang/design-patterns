6ec4d23832dcaf9acac03eef6d0272cb9eb5ac0d
magicliang <liangchuan@meituan.com>
2019-09-12 19:09:51 +0800

    升级testng到具体依赖中

---
+++ src/main/java/com/magicliang/patterns/structrural/bridge/AbstractComputer.java
@@ -0,0 +1,15 @@
+package com.magicliang.patterns.structrural.bridge;
+
+/**
+ * project name: design-patterns
+ * <p>
+ * description: 抽象电脑
+ *
+ * @author magicliang
+ * <p>
+ * date: 2019-09-12 19:08
+ */
+public abstract class AbstractComputer {
+
+    public abstract void caculate();
+}
---
+++ src/main/java/com/magicliang/patterns/structrural/bridge/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * project name: design-patterns
+ * <p>
+ * description: 桥接模式
+ * decouple an abstraction from its implementation so that the two can vary independently
+ * 桥接模式特别像对象适配器模式
+ *
+ * @author magicliang
+ * <p>
+ * date: 2019-09-12 19:00
+ */
+package com.magicliang.patterns.structrural.bridge;
