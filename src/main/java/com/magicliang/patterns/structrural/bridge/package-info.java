b49a96ac868b60167758ba84b9e1800210847d57
magicliang <liangchuan@meituan.com>
2019-09-16 21:52:09 +0800

    增加"代理模式"

--- src/main/java/com/magicliang/patterns/structrural/bridge/package-info.java
+++ src/main/java/com/magicliang/patterns/structrural/bridge/package-info.java
@@ -3,7 +3,7 @@
  * <p>
  * description: 桥接模式
  * decouple an abstraction from its implementation so that the two can vary independently
- * 桥接模式特别像对象适配器模式
+ * 桥接模式特别像对象适配器模式，但它提供了多种维度的变化，而适配器模式的外层对象是无变化的。它其实是需要多继承时的 alternative。
  *
  * @author magicliang
  * <p>
---
+++ src/main/java/com/magicliang/patterns/structrural/proxy/Subject.java
@@ -0,0 +1,18 @@
+package com.magicliang.patterns.structrural.proxy;
+
+/**
+ * project name: design-patterns
+ * <p>
+ * description: 客体接口
+ *
+ * @author magicliang
+ * <p>
+ * date: 2019-09-16 21:45
+ */
+public interface Subject {
+
+    /**
+     * 请求接口
+     */
+    void request();
+}
