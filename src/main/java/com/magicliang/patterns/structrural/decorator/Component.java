a596ff3fc4e155b744316b956496155d7a418d42
magicliang <liangchuan@meituan.com>
2019-09-16 15:55:42 +0800

    增加"装饰器模式"

---
+++ src/main/java/com/magicliang/patterns/structrural/decorator/Component.java
@@ -0,0 +1,20 @@
+package com.magicliang.patterns.structrural.decorator;
+
+/**
+ * project name: design-patterns
+ * <p>
+ * description: 基础组件接口
+ *
+ * @author magicliang
+ * <p>
+ * date: 2019-09-16 15:13
+ */
+public interface Component {
+
+    /**
+     * 基础操作接口
+     *
+     * @return 操作结果
+     */
+    boolean operation();
+}
---
+++ src/main/java/com/magicliang/patterns/structrural/decorator/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * project name: design-patterns
+ * <p>
+ * description: 装饰器模式
+ *
+ * @author magicliang
+ * <p>
+ * date: 2019-09-16 15:11
+ */
+package com.magicliang.patterns.structrural.decorator;
