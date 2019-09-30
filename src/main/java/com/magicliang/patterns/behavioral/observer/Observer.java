10f5499392f5d84880459a735b9934d74aa05fb1
magicliang <liangchuan@meituan.com>
2019-10-01 00:13:51 +0800

    增加注释

--- src/main/java/com/magicliang/patterns/behavioral/observer/Observer.java
+++ src/main/java/com/magicliang/patterns/behavioral/observer/Observer.java
@@ -5,6 +5,11 @@ package com.magicliang.patterns.behavioral.observer;
  * <p>
  * description: 观察者模式
  *
+ * java 自带
+ *  java.util.Observer and java.util.Observable 两个接口
+ *
+ *  不过自 java 9 以后就被废弃了
+ *
  * @author magicliang
  * <p>
  * date: 2019-09-30 20:09
