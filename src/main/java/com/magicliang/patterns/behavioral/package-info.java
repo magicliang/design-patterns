5691346d355bb3fba67583851c4d908bce070718
magicliang <liangchuan@meituan.com>
2021-12-05 11:19:37 +0800

    增加关于心法的注释

--- src/main/java/com/magicliang/patterns/behavioral/package-info.java
+++ src/main/java/com/magicliang/patterns/behavioral/package-info.java
@@ -2,6 +2,8 @@
  * project name: design-patterns
  * <p>
  * description:
+ * 心法：行为型模式关注对复杂算法各个步骤的抽象，关注对象之间的通信对各个对象的影响。
+ * <p>
  * 状态模式的特点是把状态实体化了，正如迭代器把下表实体化了、备忘录把状态延长化了、责任链把后继的链实体化了。
  *
  * @author magicliang
