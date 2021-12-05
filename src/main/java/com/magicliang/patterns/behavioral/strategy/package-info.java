ed81e0594ab63c298e7f8a4f92555d6378e7a856
magicliang <liangchuan@meituan.com>
2021-12-05 11:26:31 +0800

    增加心法注释

---
+++ src/main/java/com/magicliang/patterns/behavioral/strategy/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * project name: design-patterns
+ * <p>
+ * description:
+ * 心法：改变内容
+ * @author magicliang
+ * <p>
+ * date: 2021-12-05 11:25
+ */
+package com.magicliang.patterns.behavioral.strategy;
--- src/main/java/com/magicliang/patterns/behavioral/template/package-info.java
+++ src/main/java/com/magicliang/patterns/behavioral/template/package-info.java
@@ -2,7 +2,7 @@
  * project name: design-patterns
  * <p>
  * description: 模板方法，本质上就是父类声明步骤，而子类定义细节。
- *
+ * 心法：一切行为型模式的典范，定义抽象的算法，关心各个步骤之间的通信和各个步骤之间的影响。
  * @author magicliang
  * <p>
  * date: 2020-08-15 14:44
