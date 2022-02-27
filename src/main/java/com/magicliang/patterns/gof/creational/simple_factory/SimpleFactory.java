a118af46f5887f858abd6d714a67af2fb739ed80
liangchuan <liangchuan@meituan.com>
2022-02-27 15:06:22 +0800

    增加对生命周期和资源管理的注释

--- src/main/java/com/magicliang/patterns/gof/creational/simple_factory/SimpleFactory.java
+++ src/main/java/com/magicliang/patterns/gof/creational/simple_factory/SimpleFactory.java
@@ -10,6 +10,8 @@ import com.magicliang.model.Shoe;
  * <p>
  * description: 简单工厂模式，无法使用泛型
  * Spring 中的 static factory method 都使用这种模式
+ * 消耗资源的操作不应该放在构造器里执行。
+ * 应该单独使用 init-destroy 类的方法管理这些指责，而且明确根据容器的生命周期来管理
  *
  * @author magicliang
  * date: 2019-09-10 17:23
